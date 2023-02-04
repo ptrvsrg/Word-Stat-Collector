84465f7186ddee1ed9e7b47ab8f51e4b4414deda
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-04 19:34:33 +0700

    Add CSV building

---
+++ src/CsvBuilder.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Map;
+import java.util.Set;
+
+public class CsvBuilder
+{
+    private final OutputStreamWriter csvWriter;
+
+    public CsvBuilder(OutputStreamWriter outputStreamWriter)
+    {
+        csvWriter = outputStreamWriter;
+    }
+
+    public void build(Set <Map.Entry <String, Long>> set,
+                      long wordCount)
+            throws IOException
+    {
+        csvWriter.write("Word,Frequency,Frequency percentage\n");
+        for (Map.Entry <String, Long> elem : set)
+        {
+            double frequencyPercentage = (double) elem.getValue() * 100 / wordCount;
+            csvWriter.write(elem.getKey() + ",");
+            csvWriter.write(elem.getValue() + ",");
+            csvWriter.write(String.format("%.3f", frequencyPercentage) + "\n");
+            csvWriter.flush();
+        }
+    }
+}
