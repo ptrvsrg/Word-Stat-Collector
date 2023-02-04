1c8a4c9d71f8f5a51bb77fecb53018e07dc5f11c
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-04 19:32:15 +0700

    Add word collector

---
+++ src/WordCollector.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class WordCollector
+{
+    private long wordCount = 0;
+    private boolean hasHistogram = false;
+    private final InputStreamReader textReader;
+
+    public WordCollector(InputStreamReader inputStreamReader)
+    {
+        textReader = inputStreamReader;
+    }
+
+    public long getWordCount()
+    {
+        if (!hasHistogram)
+            throw new UnsupportedOperationException("The operation is not supported. " + "Histogram was not received");
+        return wordCount;
+    }
+
+    public HashMap <String, Long> getHistogram()
+            throws IOException
+    {
+        HashMap <String, Long> histogram = new HashMap <>();
+        StringBuilder word = new StringBuilder();
+        int symCode;
+
+        while ((symCode = textReader.read()) != -1)
+        {
+            if (Character.isLetterOrDigit(symCode))
+                word.append((char) symCode);
+            else if (!word.isEmpty())
+                addHistogram(histogram,
+                             word);
+        }
+
+        if (!word.isEmpty())
+            addHistogram(histogram,
+                         word);
+
+        hasHistogram = true;
+        return histogram;
+    }
+
+    private void addHistogram(HashMap <String, Long> histogram,
+                              StringBuilder word)
+    {
+        histogram.compute(new String(word).toLowerCase(),
+                          (key, value) -> (value == null) ? 1 : value + 1);
+        word.setLength(0);
+        ++wordCount;
+    }
+}
---
+++ test/WordCollectorTest.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Stream;
+
+class WordCollectorTest
+{
+
+    private static Stream <Arguments> getHistogramArgs()
+    {
+        return Stream.of(Arguments.of(new InputStreamReader(new ByteArrayInputStream("a b c a b a".getBytes())),
+                                      Arrays.asList("a",
+                                                    "b",
+                                                    "c"),
+                                      Arrays.asList(3L,
+                                                    2L,
+                                                    1L)),
+                         Arguments.of(new InputStreamReader(new ByteArrayInputStream("".getBytes())),
+                                      new ArrayList <String>(),
+                                      new ArrayList <Long>()),
+                         Arguments.of(new InputStreamReader(new ByteArrayInputStream("a A a bB c C1 43231".getBytes())),
+                                      Arrays.asList("a",
+                                                    "bb",
+                                                    "c",
+                                                    "c1",
+                                                    "43231"),
+                                      Arrays.asList(3L,
+                                                    1L,
+                                                    1L,
+                                                    1L,
+                                                    1L)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getHistogramArgs")
+    void getHistogram(InputStreamReader reader,
+                      List <String> keys,
+                      List <Long> values)
+    {
+        WordCollector wordCollector = new WordCollector(reader);
+        Map <String, Long> actual = null;
+
+        try
+        {
+            actual = wordCollector.getHistogram();
+        }
+        catch (IOException ex)
+        {
+            Assertions.fail(ex.getLocalizedMessage());
+        }
+
+        Map <String, Long> expected = new HashMap <>();
+        for (int i = 0; i < keys.size(); ++i)
+            expected.put(keys.get(i),
+                         values.get(i));
+
+        Assertions.assertArrayEquals(actual.entrySet()
+                                           .toArray(),
+                                     expected.entrySet()
+                                             .toArray(),
+                                     "Actual: " + actual + "\n Expected: " + expected);
+    }
+
+    @Test
+    void getWordCount()
+    {
+        WordCollector wordCollector = new WordCollector(new InputStreamReader(new ByteArrayInputStream("a b c a b a".getBytes())));
+        Assertions.assertThrows(UnsupportedOperationException.class,
+                                wordCollector::getWordCount);
+        try
+        {
+            wordCollector.getHistogram();
+        }
+        catch (IOException ex)
+        {
+            Assertions.fail(ex.getLocalizedMessage());
+        }
+
+        long actual = wordCollector.getWordCount();
+        long expected = 6L;
+
+        Assertions.assertEquals(actual,
+                                expected);
+    }
+}
