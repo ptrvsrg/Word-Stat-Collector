f267d5e8c1f10df373573a28917469adfecd9094
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-04 19:34:12 +0700

    Add map sorting and convert to set

---
+++ src/Sorter.java
@@ -0,0 +1,45 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Sorter
+{
+    public enum SortingType
+    {
+        ASCENDING,
+        DESCENDING
+    }
+
+    public static SortedSet <Map.Entry <String, Long>> mapSortingByValue(Map <String, Long> map,
+                                                                         SortingType sortingType)
+    {
+        Comparator <Map.Entry <String, Long>> comparator = null;
+
+        switch (sortingType)
+        {
+            case ASCENDING:
+                comparator = (o1, o2) ->
+                {
+                    if (o1.getValue().compareTo(o2.getValue()) == 0)
+                        return o1.getKey().compareTo(o2.getKey());
+
+                    return o1.getValue().compareTo(o2.getValue());
+                };
+                break;
+            case DESCENDING:
+                comparator = (o1, o2) ->
+                {
+                    if (o2.getValue().compareTo(o1.getValue()) == 0)
+                        return o1.getKey().compareTo(o2.getKey());
+
+                    return o2.getValue().compareTo(o1.getValue());
+                };
+                break;
+        }
+
+        SortedSet <Map.Entry <String, Long>> sorted = new TreeSet <>(comparator);
+        sorted.addAll(map.entrySet());
+        return sorted;
+    }
+}
---
+++ test/SorterTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+class SorterTest
+{
+
+    private static Stream <Arguments> mapSortingByValueArgs()
+    {
+        return Stream.of(Arguments.of(Arrays.asList("Albert",
+                                                    "John",
+                                                    "Ronald",
+                                                    "Michael"),
+                                      Arrays.asList(62L,
+                                                    14L,
+                                                    31L,
+                                                    31L),
+                                      Arrays.asList(new AbstractMap.SimpleEntry <>("John",
+                                                                                   14L),
+                                                    new AbstractMap.SimpleEntry <>("Michael",
+                                                                                   31L),
+                                                    new AbstractMap.SimpleEntry <>("Ronald",
+                                                                                   31L),
+                                                    new AbstractMap.SimpleEntry <>("Albert",
+                                                                                   62L)),
+                                      Sorter.SortingType.ASCENDING),
+                         Arguments.of(Arrays.asList("Albert",
+                                                    "John",
+                                                    "Ronald",
+                                                    "Michael"),
+                                      Arrays.asList(62L,
+                                                    14L,
+                                                    31L,
+                                                    31L),
+                                      Arrays.asList(new AbstractMap.SimpleEntry <>("Albert",
+                                                                                   62L),
+                                                    new AbstractMap.SimpleEntry <>("Michael",
+                                                                                   31L),
+                                                    new AbstractMap.SimpleEntry <>("Ronald",
+                                                                                   31L),
+                                                    new AbstractMap.SimpleEntry <>("John",
+                                                                                   14L)),
+                                      Sorter.SortingType.DESCENDING));
+    }
+
+    @ParameterizedTest
+    @MethodSource("mapSortingByValueArgs")
+    void mapSortingByValue(List <String> keys,
+                           List <Long> values,
+                           List <AbstractMap.SimpleEntry <String, Long>> expected,
+                           Sorter.SortingType sortingType)
+    {
+        Map <String, Long> actualMap = new HashMap <>();
+        for (int i = 0; i < keys.size(); ++i)
+            actualMap.put(keys.get(i),
+                          values.get(i));
+
+        SortedSet <Map.Entry <String, Long>> actual = Sorter.mapSortingByValue(actualMap,
+                                                                               sortingType);
+        Assertions.assertArrayEquals(expected.toArray(),
+                                     actual.toArray());
+    }
+}
