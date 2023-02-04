a673788ef43aa30bc4495f35494e04e37717a8be
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-04 19:31:54 +0700

    Add command line parser

---
+++ src/ClParser.java
@@ -0,0 +1,70 @@
+import org.apache.commons.cli.*;
+
+public class ClParser
+{
+    private CommandLine commandLine;
+
+    public ClParser(String[] args)
+            throws ParseException
+    {
+        Options opts = new Options();
+        opts.addOption(Option.builder()
+                             .option("h")
+                             .longOpt("help")
+                             .desc("Print command help")
+                             .build());
+
+        CommandLineParser clParser = new DefaultParser();
+        commandLine = clParser.parse(opts,
+                                     args,
+                                     true);
+
+        opts.addOption(Option.builder()
+                             .option("i")
+                             .longOpt("input")
+                             .desc("Input file")
+                             .hasArg()
+                             .argName("path")
+                             .numberOfArgs(1)
+                             .type(String.class)
+                             .required()
+                             .build())
+            .addOption(Option.builder()
+                             .option("o")
+                             .longOpt("output")
+                             .desc("Output csv file")
+                             .hasArg()
+                             .argName("path")
+                             .numberOfArgs(1)
+                             .type(String.class)
+                             .required()
+                             .build());
+
+        if (commandLine.hasOption("help"))
+        {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("app",
+                                    "Available command:",
+                                    opts,
+                                    "",
+                                    true);
+            if (commandLine.getOptions().length == 1)
+                return;
+        }
+
+        commandLine = clParser.parse(opts,
+                                     args);
+    }
+
+    public Object getInput()
+            throws ParseException
+    {
+        return commandLine.getParsedOptionValue("input");
+    }
+
+    public Object getOutput()
+            throws ParseException
+    {
+        return commandLine.getParsedOptionValue("output");
+    }
+}
---
+++ test/ClParserTest.java
@@ -0,0 +1,68 @@
+import org.apache.commons.cli.ParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class ClParserTest
+{
+    private static Stream <Arguments> getInputArgs()
+    {
+        return Stream.of(Arguments.of(new String[]{"--input", "path", "--output", "path.csv"},
+                                      "path",
+                                      "path.csv",
+                                      false),
+                         Arguments.of(new String[]{"--output", "path.csv"},
+                                      "",
+                                      "path.csv",
+                                      true),
+                         Arguments.of(new String[]{"--input", "path"},
+                                      "path",
+                                      "",
+                                      true),
+                         Arguments.of(new String[]{"--input", "path", "--output", "path.csv", "--config", "path.json"},
+                                      "path",
+                                      "path.csv",
+                                      true),
+                         Arguments.of(new String[]{"--input", "path", "--output", "path.csv", "--help"},
+                                      "path",
+                                      "path.csv",
+                                      false));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getInputArgs")
+    void getInputOutput(String[] args,
+                        String expectedInput,
+                        String expectedOutput,
+                        boolean exception)
+    {
+        try
+        {
+            ClParser clParser = new ClParser(args);
+            Assertions.assertEquals(clParser.getInput(),
+                                    expectedInput,
+                                    "Incorrect input file");
+            Assertions.assertEquals(clParser.getOutput(),
+                                    expectedOutput,
+                                    "Incorrect output file");
+        }
+        catch (ParseException ex)
+        {
+            if (!exception)
+                Assertions.fail();
+        }
+    }
+
+    @Test
+    void printHelp()
+    {
+        Assertions.assertDoesNotThrow(() ->
+                                      {new ClParser(new String[]{"--help"});});
+        Assertions.assertDoesNotThrow(() ->
+                                      {new ClParser(new String[]{"--help", "--mode", "dark"});});
+    }
+}
