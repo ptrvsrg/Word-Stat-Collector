3a3cf778b41b9b86c50d2b661b12613904e5805c
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-06 16:32:43 +0700

    Delete 2 sorting mode

--- src/main/java/ru/nsu/fit/oop/task1/Sorter.java
+++ src/main/java/ru/nsu/fit/oop/task1/Sorter.java
@@ -7,38 +7,15 @@ import java.util.TreeSet;
 
 public class Sorter
 {
-    public enum SortingType
+    public static SortedSet <Map.Entry <String, Long>> mapSortingByValue(Map <String, Long> map)
     {
-        ASCENDING,
-        DESCENDING
-    }
-
-    public static SortedSet <Map.Entry <String, Long>> mapSortingByValue(Map <String, Long> map,
-                                                                         SortingType sortingType)
-    {
-        Comparator <Map.Entry <String, Long>> comparator = null;
-
-        switch (sortingType)
+        Comparator <Map.Entry <String, Long>> comparator = (o1, o2) ->
         {
-            case ASCENDING:
-                comparator = (o1, o2) ->
-                {
-                    if (o1.getValue().compareTo(o2.getValue()) == 0)
-                        return o1.getKey().compareTo(o2.getKey());
+            if (o1.getValue().compareTo(o2.getValue()) == 0)
+                return o1.getKey().compareTo(o2.getKey());
 
-                    return o1.getValue().compareTo(o2.getValue());
-                };
-                break;
-            case DESCENDING:
-                comparator = (o1, o2) ->
-                {
-                    if (o2.getValue().compareTo(o1.getValue()) == 0)
-                        return o1.getKey().compareTo(o2.getKey());
-
-                    return o2.getValue().compareTo(o1.getValue());
-                };
-                break;
-        }
+            return o2.getValue().compareTo(o1.getValue());
+        };
 
         SortedSet <Map.Entry <String, Long>> sorted = new TreeSet <>(comparator);
         sorted.addAll(map.entrySet());
