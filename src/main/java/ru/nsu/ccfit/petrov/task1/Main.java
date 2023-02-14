33333663933b74958de2870ac9073860e4714dc2
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-14 16:20:15 +0700

    Update main

--- src/main/java/ru/nsu/ccfit/petrov/task1/Main.java
+++ src/main/java/ru/nsu/ccfit/petrov/task1/Main.java
@@ -3,35 +3,35 @@ package ru.nsu.ccfit.petrov.task1;
 import org.apache.commons.cli.ParseException;
 
 import java.io.*;
-import java.util.Map;
-import java.util.SortedSet;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        CommandLineParser clParser = new CommandLineParser();
+
         try
         {
-            CommandLineParser clParser = new CommandLineParser();
             if (!clParser.parse(args))
                 return;
-
-            String inputFile = clParser.getInput();
-            String outputFile = clParser.getOutput();
-
-            try (InputStreamReader inputStreamReader = new FileReader(inputFile);
-                 OutputStreamWriter outputStreamWriter = new FileWriter(outputFile, false))
-            {
-                WordCollector wordCollector = new WordCollector(inputStreamReader);
-                Map <String, Long> histogram = wordCollector.getHistogram();
-                SortedSet <Map.Entry <String, Long>> sortedSet = Sorter.mapSortingByValue(histogram);
-                CsvBuilder csvBuilder = new CsvBuilder(outputStreamWriter);
-                csvBuilder.build(sortedSet,
-                                 wordCollector.getWordCount());
-            }
         }
-        catch (ParseException |
-               IOException ex)
+        catch (ParseException ex)
+        {
+            ex.printStackTrace(System.err);
+            return;
+        }
+
+        String inputFile = clParser.getInput();
+        String outputFile = clParser.getOutput();
+
+        try (WordCollector wordCollector = new WordCollector(new FileReader(inputFile));
+             CsvBuilder csvBuilder = new CsvBuilder(new FileWriter(outputFile, false)))
+        {
+            SortedHistogram histogram = wordCollector.getHistogram();
+            csvBuilder.build(histogram,
+                             wordCollector.getWordCount());
+        }
+        catch (IOException ex)
         {
             ex.printStackTrace(System.err);
         }
